4c1d40095da38da91ca0f04bde1ace3cb34e3b65
Keithclinton <keithnyaburi@gmail.com>
2024-07-18 16:32:05 +0300

    blog should list all posts, allow logged in users

--- __innit__.py
+++ __innit__.py
@@ -33,3 +33,9 @@ def create_app(test_config=None):
         app.register_blueprint(auth.bp)
 
     return app
+def create_app():
+    app = ...
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+    return app
